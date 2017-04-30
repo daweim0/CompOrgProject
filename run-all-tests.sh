05fc0690b5db5408f2904eafac8740b4dfba19a4
Sean Rice <seane.rice@gmail.com>
2017-04-30 02:03:30 -0400

    Outputs are now in "not-taken.out.txt" and "taken.out.txt"

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 echo Taken:
 echo 1
-./iplc-sim < Inputs/input-taken-7-1-1.txt > Outputs/taken-7-1-1.out.txt
+./iplc-sim < Inputs/input-taken-7-1-1.txt > Outputs/taken.out.txt
 echo 2
-./iplc-sim < Inputs/input-taken-6-1-2.txt > Outputs/taken-6-1-2.out.txt
+./iplc-sim < Inputs/input-taken-6-1-2.txt >> Outputs/taken.out.txt
 echo 3
-./iplc-sim < Inputs/input-taken-5-1-4.txt > Outputs/taken-5-1-4.out.txt
+./iplc-sim < Inputs/input-taken-5-1-4.txt >> Outputs/taken.out.txt
 echo 4
-./iplc-sim < Inputs/input-taken-6-2-1.txt > Outputs/taken-6-2-1.out.txt
+./iplc-sim < Inputs/input-taken-6-2-1.txt >> Outputs/taken.out.txt
 echo 5
-./iplc-sim < Inputs/input-taken-5-2-2.txt > Outputs/taken-5-2-2.out.txt
+./iplc-sim < Inputs/input-taken-5-2-2.txt >> Outputs/taken.out.txt
 echo 6
-./iplc-sim < Inputs/input-taken-4-2-4.txt > Outputs/taken-4-2-4.out.txt
+./iplc-sim < Inputs/input-taken-4-2-4.txt >> Outputs/taken.out.txt
 echo 7
-./iplc-sim < Inputs/input-taken-6-4-1.txt > Outputs/taken-6-4-1.out.txt
+./iplc-sim < Inputs/input-taken-6-4-1.txt >> Outputs/taken.out.txt
 echo 8
-./iplc-sim < Inputs/input-taken-5-4-2.txt > Outputs/taken-5-4-2.out.txt
+./iplc-sim < Inputs/input-taken-5-4-2.txt >> Outputs/taken.out.txt
 echo 9
-./iplc-sim < Inputs/input-taken-4-4-4.txt > Outputs/taken-4-4-4.out.txt
+./iplc-sim < Inputs/input-taken-4-4-4.txt >> Outputs/taken.out.txt
 
 echo Not-Taken:
 echo 1
-./iplc-sim < Inputs/input-not-taken-7-1-1.txt > Outputs/not-taken-7-1-1.out.txt
+./iplc-sim < Inputs/input-not-taken-7-1-1.txt > Outputs/not-taken.out.txt
 echo 2
-./iplc-sim < Inputs/input-not-taken-6-1-2.txt > Outputs/not-taken-6-1-2.out.txt
+./iplc-sim < Inputs/input-not-taken-6-1-2.txt >> Outputs/not-taken.out.txt
 echo 3
-./iplc-sim < Inputs/input-not-taken-5-1-4.txt > Outputs/not-taken-5-1-4.out.txt
+./iplc-sim < Inputs/input-not-taken-5-1-4.txt >> Outputs/not-taken.out.txt
 echo 4
-./iplc-sim < Inputs/input-not-taken-6-2-1.txt > Outputs/not-taken-6-2-1.out.txt
+./iplc-sim < Inputs/input-not-taken-6-2-1.txt >> Outputs/not-taken.out.txt
 echo 5
-./iplc-sim < Inputs/input-not-taken-5-2-2.txt > Outputs/not-taken-5-2-2.out.txt
+./iplc-sim < Inputs/input-not-taken-5-2-2.txt >> Outputs/not-taken.out.txt
 echo 6
-./iplc-sim < Inputs/input-not-taken-4-2-4.txt > Outputs/not-taken-4-2-4.out.txt
+./iplc-sim < Inputs/input-not-taken-4-2-4.txt >> Outputs/not-taken.out.txt
 echo 7
-./iplc-sim < Inputs/input-not-taken-6-4-1.txt > Outputs/not-taken-6-4-1.out.txt
+./iplc-sim < Inputs/input-not-taken-6-4-1.txt >> Outputs/not-taken.out.txt
 echo 8
-./iplc-sim < Inputs/input-not-taken-5-4-2.txt > Outputs/not-taken-5-4-2.out.txt
+./iplc-sim < Inputs/input-not-taken-5-4-2.txt >> Outputs/not-taken.out.txt
 echo 9
-./iplc-sim < Inputs/input-not-taken-4-4-4.txt > Outputs/not-taken-4-4-4.out.txt
+./iplc-sim < Inputs/input-not-taken-4-4-4.txt >> Outputs/not-taken.out.txt
