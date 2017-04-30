76731969ae05cfe0b21bb852a45ab63a2f430513
Sean Rice <seane.rice@gmail.com>
2017-04-30 02:11:43 -0400

    Pretty script, pretty output

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
 echo Taken:
-echo 1
+echo Processing...
 ./iplc-sim < Inputs/input-taken-7-1-1.txt > Outputs/taken.out.txt
-echo 2
 ./iplc-sim < Inputs/input-taken-6-1-2.txt >> Outputs/taken.out.txt
-echo 3
 ./iplc-sim < Inputs/input-taken-5-1-4.txt >> Outputs/taken.out.txt
-echo 4
 ./iplc-sim < Inputs/input-taken-6-2-1.txt >> Outputs/taken.out.txt
-echo 5
 ./iplc-sim < Inputs/input-taken-5-2-2.txt >> Outputs/taken.out.txt
-echo 6
 ./iplc-sim < Inputs/input-taken-4-2-4.txt >> Outputs/taken.out.txt
-echo 7
 ./iplc-sim < Inputs/input-taken-6-4-1.txt >> Outputs/taken.out.txt
-echo 8
 ./iplc-sim < Inputs/input-taken-5-4-2.txt >> Outputs/taken.out.txt
-echo 9
 ./iplc-sim < Inputs/input-taken-4-4-4.txt >> Outputs/taken.out.txt
-
+echo
 echo Not-Taken:
-echo 1
+echo Processing...
 ./iplc-sim < Inputs/input-not-taken-7-1-1.txt > Outputs/not-taken.out.txt
-echo 2
 ./iplc-sim < Inputs/input-not-taken-6-1-2.txt >> Outputs/not-taken.out.txt
-echo 3
 ./iplc-sim < Inputs/input-not-taken-5-1-4.txt >> Outputs/not-taken.out.txt
-echo 4
 ./iplc-sim < Inputs/input-not-taken-6-2-1.txt >> Outputs/not-taken.out.txt
-echo 5
 ./iplc-sim < Inputs/input-not-taken-5-2-2.txt >> Outputs/not-taken.out.txt
-echo 6
 ./iplc-sim < Inputs/input-not-taken-4-2-4.txt >> Outputs/not-taken.out.txt
-echo 7
 ./iplc-sim < Inputs/input-not-taken-6-4-1.txt >> Outputs/not-taken.out.txt
-echo 8
 ./iplc-sim < Inputs/input-not-taken-5-4-2.txt >> Outputs/not-taken.out.txt
-echo 9
 ./iplc-sim < Inputs/input-not-taken-4-4-4.txt >> Outputs/not-taken.out.txt
+echo
+echo "Output files stored in:"
+echo "\t Outputs/taken.out.txt"
+echo "\t Outputs/not-taken.out.txt"
